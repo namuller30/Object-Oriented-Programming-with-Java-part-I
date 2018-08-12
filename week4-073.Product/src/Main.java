b26e715db29b970e05033015e103a76305e4dd99
namuller30 <nate.muller30@gmail.com>
2018-08-12 14:57:46 -0500

    Add files via upload

---
+++ week4-073.Product/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // You can test your new class here, try e.g.:
+        Product test = new Product("Test",10,11);
+        test.printProduct();
+        // Product t = new Product("Banana", 1.1, 13);
+        // t.printProduct();
+    }
+}
