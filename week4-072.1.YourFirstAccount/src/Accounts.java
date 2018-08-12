5d4a7ae219a216ba1cac95bf149d0272b858a042
namuller30 <nate.muller30@gmail.com>
2018-08-12 14:55:36 -0500

    Add files via upload

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        Account firstAccount = new Account("First Account",100.0);
+        firstAccount.deposit(20);
+        System.out.println(firstAccount.toString());
+        
+        
+                
+        
+        
+        
+    }
+
+}
